9cbf282b36ff7006c015a9a99c9fef43f860cf77
tOñO <antvhell@hotmail.com>
2023-09-07 16:47:11 -0600

    Configurando gulp para los estilos

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,39 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, series, parallel } = require("gulp");
+
+// CSS Y SASS
 const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+// Imagenes
+// const imagemin = require("gulp-imagemin");
+// import imagemin from "gulp-imagemin";
 
 function css(done) {
   // Compilar sass
   // paso 1 - Identificar archivo, 2 - Compilarla, 3 - Guardar e. .css
 
-  src("src/scss/app.scss").pipe(sass()).pipe(dest("build/css"));
+  src("src/scss/app.scss")
+    //compressed o expanded
+    .pipe(sass())
+    .pipe(postcss([autoprefixer]))
+    .pipe(dest("build/css"));
   done();
 }
 
+// function imagenes() {
+//   return gulp.src("src/img/**/*").pipe(imagemin()).pipe(gulp.dest("build/img"));
+// }
+
 function dev() {
-  watch("src/scss/app.scss", css);
+  watch("src/scss/**/*.scss", css);
+  // watch("src/img/**/*", imagenes);
 }
 
 exports.css = css;
 exports.dev = dev;
+// exports.imagenes = imagenes;
+exports.default = series(css, dev);
+
+// series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+// parallel - Todas inician al mismo tiempo
